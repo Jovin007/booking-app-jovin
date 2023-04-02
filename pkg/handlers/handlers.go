1048c1212636067bfae22bce3087df0b1cda4a79
Jovin Joseph <jovinjjoseph@gmail.com>
2023-04-02 20:40:36 +0530

    New changes

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,10 +1,14 @@
 package handlers
 
 import (
+	"booking-app-jovin/config"
 	"booking-app-jovin/pkg/render"
+	"database/sql"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"strings"
 )
 
 // Home is the handler for the home page
@@ -22,13 +26,61 @@ func GetEmail(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "get-email.page.tmpl")
 }
 func ValidateEmail(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method) //get request method
+	//fmt.Println("method:", r.Method) //get request method
 	if r.Method == "GET" {
 		t, _ := template.ParseFiles("get-email.page.tmpl")
 		t.Execute(w, nil)
 	} else {
 		r.ParseForm()
 		// logic part of log in
-		fmt.Println("Email-Id:", r.Form["email"])
+		//fmt.Println("Email-Id:", r.Form["email"])
+		clientEmail := strings.Join(r.Form["email"], "")
+		if IsEmailIdRegistered(clientEmail) {
+			fmt.Println("Client Registered!!")
+
+		} else {
+			fmt.Println("Client Not Registered!!")
+		}
+
+	}
+}
+
+func IsEmailIdRegistered(email string) bool {
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		config.Host, config.Port, config.User, config.Password, config.Dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(email)
+	rows, err := db.Query("SELECT COUNT(*) FROM client_details where email_id=$1", email)
+
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	count := checkCount(rows)
+	rows.Close()
+	var isRegistered bool = false
+	if count >= 1 {
+		isRegistered = true
+	}
+	return isRegistered
+
+}
+func checkCount(rows *sql.Rows) (count int) {
+	for rows.Next() {
+		err := rows.Scan(&count)
+		checkErr(err)
+	}
+	return count
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
